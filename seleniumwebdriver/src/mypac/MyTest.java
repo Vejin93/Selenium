5af0472151ceeb18390e5583a2a5a1f94dbcdc0b
Vladimir Vejin <vejin.vladimir@gmail.com>
2022-12-07 22:15:53 +0100

    First commit

---
+++ seleniumwebdriver/src/mypac/MyTest.java
@@ -0,0 +1,8 @@
+package mypac;
+
+public class MyTest {
+    public static void main(String[] args){
+
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Sladja\\Desktop\\chromedriver.exe");
+    }
+}
