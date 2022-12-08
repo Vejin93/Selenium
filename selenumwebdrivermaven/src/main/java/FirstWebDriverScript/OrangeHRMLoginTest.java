15f87d12eac8628054afc9f912e127b451670a2b
Vladimir Vejin <vejin.vladimir@gmail.com>
2022-12-08 21:24:50 +0100

    Add some code

--- selenumwebdrivermaven/src/main/java/FirstWebDriverScript/OrangeHRMLoginTest.java
+++ selenumwebdrivermaven/src/main/java/FirstWebDriverScript/OrangeHRMLoginTest.java
@@ -1,6 +1,7 @@
 package FirstWebDriverScript;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -25,19 +26,19 @@ public class OrangeHRMLoginTest {
 
         //1) Launch browser
         //ChromeDriver driver  = new ChromeDriver();
-        WebDriver driver  = new ChromeDriver();
+        WebDriver driver = new ChromeDriver();
 
         //2)Open URL on the browser
         driver.get("https://opensource-demo.orangehrmlive.com/");
-            //Open full window
+        //Open full window
         driver.manage().window().maximize();
         Thread.sleep(5000);
 
         //3) Provide username
-            //WebElement txtUsername = driver.findElement(By.name("username"));
-            //txtUsername.sendKeys("Admin");
+        //WebElement txtUsername = driver.findElement(By.name("username"));
+        //txtUsername.sendKeys("Admin");
 
-            //Another way in one line(shorter)
+        //Another way in one line(shorter)
         driver.findElement(By.name("username")).sendKeys("Admin");
 
         //4) Provide password
@@ -47,15 +48,31 @@ public class OrangeHRMLoginTest {
 
         //5) Click on submit button
         driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
-
+        Thread.sleep(5000);
         //6) Verify the title of dashboard
-        String act_title = driver.getTitle();
-        String exp_title = "OrangeHRM";
-        //String exp_title = "OrangeHRM123";
-        if(act_title.equals(exp_title)){
+
+//        String act_title = driver.getTitle();
+//        String exp_title = "OrangeHRM";
+//        //String exp_title = "OrangeHRM123";
+//        if(act_title.equals(exp_title)){
+//            System.out.println("Test passed!");
+//        }else{
+//            System.out.println("Failed.");
+//        }
+        //Title validation
+        //Label validation after successful login
+        String act_label = null;
+        try{
+        act_label = driver.findElement(By.xpath("/html/body/div/div[1]/div[1]/header/div[1]/div[1]/span/h6")).getText();
+        }catch (NoSuchElementException e){
+            act_label="";
+        }
+        String exp_label = "Dashboard";
+
+        if(act_label.equals(exp_label)){
             System.out.println("Test passed!");
         }else{
-            System.out.println("Failed.");
+            System.out.println("Test failed.");
         }
 
         //7) close browser
