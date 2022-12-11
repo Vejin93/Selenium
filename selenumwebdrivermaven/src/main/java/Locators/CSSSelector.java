a06d3d85dd051737fa537ac0e8c14c617083abad
Vladimir Vejin <vejin.vladimir@gmail.com>
2022-12-11 12:29:23 +0100

    Add code for cssLocator, for CSS Tag & ID and CSS only ID

--- selenumwebdrivermaven/src/main/java/Locators/CSSSelector.java
+++ selenumwebdrivermaven/src/main/java/Locators/CSSSelector.java
@@ -1,7 +1,22 @@
 package Locators;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CSSSelector {
     public static void main(String[] args){
-        
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://demo.nopcommerce.com/");
+        driver.manage().window().maximize();
+
+        //CSS with tag & id
+        driver.findElement(By.cssSelector("input#small-searchterms")).sendKeys("MacBook");
+        //CSS with id
+        driver.findElement(By.cssSelector("#small-searchterms")).sendKeys("MacBook");
+
+
     }
 }
