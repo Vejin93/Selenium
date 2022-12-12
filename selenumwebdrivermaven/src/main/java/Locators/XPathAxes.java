280044360bb0071e100984779aae69e6ff7c5f7c
Vladimir Vejin <vejin.vladimir@gmail.com>
2022-12-12 22:47:52 +0100

    Add code for Anchestor, Descendant, Following, Following-Sibiling, Presceding and presceding-sibiling for XPathAxes

--- selenumwebdrivermaven/src/main/java/Locators/XPathAxes.java
+++ selenumwebdrivermaven/src/main/java/Locators/XPathAxes.java
@@ -26,5 +26,31 @@ public class XPathAxes {
         //Child - Selects all children of the current node (One or many)
         List<WebElement> childs = driver.findElements(By.xpath("//a[contains(text(),'NIIT Ltd')]/ancestor::tr/child::td"));
         System.out.println("Number of child elements:" + childs.size());
+
+        //Ancestor - Selects all ancestors (parent, grandparent, etc.)
+        text=driver.findElement(By.xpath("//a[contains(text(),'NIIT Ltd')]/ancestor::tr")).getText();
+        System.out.println("Ancestor : "+text);
+
+        //Descendant - Selects all descendants (children, grandchildren, etc.) of the current node
+        List<WebElement> descendants=driver.findElements(By.xpath("//a[contains(text(),'NIIT Ltd')]/ancestor::tr/descendant::*"));
+        System.out.println("Number of descendant nodes:"+descendants.size());
+
+        //Following -Selects everything in the document after the closing tag of the current node
+        List<WebElement>followingnodes=driver.findElements(By.xpath("//a[contains(text(),'NIIT Ltd')]/ancestor::tr/following::tr"));
+        System.out.println("Number of following nodes:"+followingnodes.size());
+
+        //Following-sibling : Selects all siblings after the current node
+        List<WebElement> followingsiblings=driver.findElements(By.xpath("//a[contains(text(),'NIIT Ltd')]/ancestor::tr/following-sibling::tr"));
+        System.out.println("Number of Following Siblings:"+followingsiblings.size());
+
+        //Preceding - Selects all nodes that appear before the current node in the document
+        List<WebElement> precedings=driver.findElements(By.xpath("//a[contains(text(),'NIIT Ltd')]/ancestor::tr/preceding::tr"));
+        System.out.println("Number of preceding nodes:"+precedings.size());
+
+        //preceding-sibling - Selects all siblings before the current node
+        List<WebElement> precedingsiblings=driver.findElements(By.xpath("//a[contains(text(),'NIIT Ltd')]/ancestor::tr/preceding-sibling::tr"));
+        System.out.println("Number of preceding sibling nodes:"+precedingsiblings.size());
+
+        driver.quit();
     }
 }
