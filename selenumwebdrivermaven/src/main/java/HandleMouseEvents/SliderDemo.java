fa8c297c68f5d8c535f71ac18651334aceb9758f
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-01-15 10:42:51 +0100

    Add code for HandleMouseEvents - SliderDemo

---
+++ selenumwebdrivermaven/src/main/java/HandleMouseEvents/SliderDemo.java
@@ -0,0 +1,36 @@
+package HandleMouseEvents;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+
+public class SliderDemo {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.jqueryscript.net/demo/Price-Range-Slider-jQuery-UI/");
+        driver.manage().window().maximize();
+
+        Actions act = new Actions(driver);
+
+        // min slider
+        WebElement min_slider = driver.findElement(By.xpath("//span[1]"));
+        System.out.println("Current location of min slider: " + min_slider.getLocation()); // (59,250)
+
+        act.dragAndDropBy(min_slider,100,250).perform();
+
+        System.out.println("Location of min slider after moving: " + min_slider.getLocation());
+
+        // max slider
+        WebElement max_slider = driver.findElement(By.xpath("//span[2]"));
+        System.out.println("Current location of max slider: " + max_slider.getLocation()); // (544,250)
+
+        act.dragAndDropBy(max_slider, -44,250).perform();
+
+        System.out.println("Location of max slider after moving: " + max_slider.getLocation());
+    }
+}
