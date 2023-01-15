d3818fafaf16be72d3a291cc83ea78cf147d63c8
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-01-15 11:30:42 +0100

    Add code for HandleMouseEvents - ActionsVsAction

---
+++ selenumwebdrivermaven/src/main/java/HandleMouseEvents/ActionsVsAction.java
@@ -0,0 +1,28 @@
+package HandleMouseEvents;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+
+public class ActionsVsAction {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://swisnl.github.io/jQuery-contextMenu/demo.html");
+        driver.manage().window().maximize();
+
+        WebElement button = driver.findElement(By.xpath("//span[@class='context-menu-one btn btn-neutral']"));
+
+        Actions act = new Actions(driver);
+
+        //act.contextClick(button).perform(); // right click
+
+        Action myAction = act.contextClick(button).build();// create action and store it in variable
+
+        myAction.perform(); // perform action from variable 'myAction'
+    }
+}
