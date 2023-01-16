90f6daffcb11ef76c49f07067188e44ee2ac69b6
Vladimir Vejin <vejin.vladimir@gmail.com>
2023-01-16 22:41:28 +0100

    Add code for JavaScriptExecutor

--- selenumwebdrivermaven/src/main/java/HandleKeyboardEventsScrollingPageTabsAndWindows/InteractWithElementsUsingJavaScriptExecutor.java
+++ selenumwebdrivermaven/src/main/java/HandleKeyboardEventsScrollingPageTabsAndWindows/InteractWithElementsUsingJavaScriptExecutor.java
@@ -1,6 +1,9 @@
 package HandleKeyboardEventsScrollingPageTabsAndWindows;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class InteractWithElementsUsingJavaScriptExecutor {
@@ -11,7 +14,26 @@ public class InteractWithElementsUsingJavaScriptExecutor {
         driver.get("https://testautomationpractice.blogspot.com/");
         driver.manage().window().maximize();
 
-        
+        driver.switchTo().frame(0);
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        // First name
+        WebElement inputBox = driver.findElement(By.id("RESULT_TextField-1"));
+        js.executeScript("arguments[0].setAttribute('value','john')",inputBox);
+
+        // Radiobutton
+        WebElement maleRadioButton = driver.findElement(By.id("RESULT_RadioButton-7_0"));
+        //maleRadioButton.click();// ClickInterceptedException
+        js.executeScript("arguments[0].click();",maleRadioButton);
+
+        // Checkbox
+        WebElement checkboxWednesday= driver.findElement(By.id("RESULT_CheckBox-8_3"));
+        js.executeScript("arguments[0].click();",checkboxWednesday);
+
+        // button
+        WebElement button = driver.findElement(By.id("FSsubmit"));
+        js.executeScript("arguments[0].click();",button);
 
     }
 }
